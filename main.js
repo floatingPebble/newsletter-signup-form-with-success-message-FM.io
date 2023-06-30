aaeaf3077c23c2946e0600c8437e8d27af21a90a
Goran Malić <goran.malic.dev@gmail.com>
2023-06-30 18:04:32 -0400

    project finished

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ if (form != null) {
     form.addEventListener('submit', e => {
 
         if (!regex.test(email.value.trim())) {
+            let errMsg = email.value.trim() === "" ? "This field cannot be empty" : "Valid email required";
+            form.setAttribute("data-errMsg", errMsg);
+
             e.preventDefault();
             form.classList.add("error");
         } else {
